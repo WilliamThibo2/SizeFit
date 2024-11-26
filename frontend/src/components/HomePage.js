46bb287f5cc11fc9e226c53f0397c89c6ae12bce
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-26 16:39:05 -0500

    Create HomePage.js

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { VStack, Heading, Button } from '@chakra-ui/react';
+
+function HomePage() {
+    return (
+        <VStack>
+            <Heading>Trouvez votre taille parfaite</Heading>
+            <Button>Commencer le test</Button>
+        </VStack>
+    );
+}
+
+export default HomePage;
